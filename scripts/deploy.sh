2f50a9aba875cbabb7a6bccf525bdd5ca1187be7
Thanh Dinh <dnthanh1298@gmail.com>
2020-08-03 22:54:09 +0700

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
-printful=$(sudo docker ps -aq --filter "name=printful")
+printful=$(docker ps -aq --filter "name=printful")
 
-isAppRunning=$(sudo docker ps -aq --filter "name=printful" --filter status=running)
+isAppRunning=$(docker ps -aq --filter "name=printful" --filter status=running)
 
 if [ "$isAppRunning" != "" ]; then
-  sudo docker stop $printful
-  sudo docker rm $printful
+    docker stop $printful
+    docker rm $printful
 fi
 
 docker login -u $1 -p $2 registry.gitlab.com
-sudo docker run --name printful -d --restart on-failure --network brandly-demo registry.gitlab.com/dnthanh1298/printful
-sudo docker image prune -f
+docker run --name printful -d --restart on-failure --network brandly-demo registry.gitlab.com/dnthanh1298/printful
+docker image prune -f
